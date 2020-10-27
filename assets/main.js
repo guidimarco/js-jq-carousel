4fae26089588f55b78dca3ca1f87565edb0d4978
Marco Guidi <mark.88g@gmail.com>
2020-10-27 16:47:36 +0100

    problema dot carosello

--- assets/main.js
+++ assets/main.js
@@ -1,44 +1,81 @@
 $("document").ready(function() {
 
     // CAROUSEL MOOVER
-    // button for moving next
-    $(".next").click(function() {
-        // VAR ASSIGNMENT: IMG AND DOT
+    // NEXT IMG
+    $(".next").click(moveToR);
+
+    // PREV IMG
+    $(".prev").click(moveToL);
+
+    // DOT MOV
+    $(".dot").click(function() {
+        // VAR ASSIGMENT current img and dot, clicked dot
+        var clickedDot = $(this);
         var currentImg = $("img.active");
         var currentDot = $(".dot.active");
-        // remove class .active in the current img and in the current dot
-        currentImg.removeClass("active");
-        currentDot.removeClass("active");
-        if (currentImg.next("img").length) {
-            // current.next() it's not empty
-            // --> add class .active in the next img and dot
-            currentImg.next("img").addClass("active");
-            currentDot.next(".dot").addClass("active");
-        } else {
-            // current.next() it's empty
-            // --> add class .active in the first img and dot
-            $(".carousel > img:first-of-type").addClass("active");
-            $(".carousel > .dots > .dot:first-of-type").addClass("active");
+        var isActive = clickedDot.hasClass("active");
+
+        console.log(isActive);
+
+        // WHILE clicked dot it's not active
+        if (!isActive) {
+            currentImg.removeClass("active");
+            currentDot.removeClass("active");
+            if (currentImg.next("img").length) {
+                // current.next() it's not empty
+                // --> add class .active in the next img and dot
+                currentImg.next("img").addClass("active");
+                currentDot.next(".dot").addClass("active");
+            } else {
+                // current.next() it's empty
+                // --> add class .active in the first img and dot
+                $(".carousel > img:first-of-type").addClass("active");
+                $(".carousel > .dots > .dot:first-of-type").addClass("active");
+            }
+            console.log(isActive);
         }
-    });
-    // button for moving prev
-    $(".prev").click(function() {
-        // VAR ASSIGNMENT: IMG AND DOT
-        var currentImg = $("img.active");
-        var currentDot = $(".dot.active");
-        // remove class .active in the current img and dot
-        currentImg.removeClass("active");
-        currentDot.removeClass("active");
-        if (currentImg.prev("img").length) {
-            // prev.next() it's not empty
-            // --> add class .active in the prev img and dot
-            currentImg.prev("img").addClass("active");
-            currentDot.prev(".dot").addClass("active");
-        } else {
-            // prev.next() it's empty
-            // --> add class .active in the last img and dot
-            $(".carousel > img:last-of-type").addClass("active");
-            $(".carousel > .dots > .dot:last-of-type").addClass("active");
-        }
+
     });
 });
+
+// move img to r
+function moveToR() {
+    // VAR ASSIGNMENT: IMG AND DOT
+    var currentImg = $("img.active");
+    var currentDot = $(".dot.active");
+    // remove class .active in the current img and in the current dot
+    currentImg.removeClass("active");
+    currentDot.removeClass("active");
+    if (currentImg.next("img").length) {
+        // current.next() it's not empty
+        // --> add class .active in the next img and dot
+        currentImg.next("img").addClass("active");
+        currentDot.next(".dot").addClass("active");
+    } else {
+        // current.next() it's empty
+        // --> add class .active in the first img and dot
+        $(".carousel > img:first-of-type").addClass("active");
+        $(".carousel > .dots > .dot:first-of-type").addClass("active");
+    }
+};
+
+// move img to l
+function moveToL() {
+    // VAR ASSIGNMENT: IMG AND DOT
+    var currentImg = $("img.active");
+    var currentDot = $(".dot.active");
+    // remove class .active in the current img and dot
+    currentImg.removeClass("active");
+    currentDot.removeClass("active");
+    if (currentImg.prev("img").length) {
+        // prev.next() it's not empty
+        // --> add class .active in the prev img and dot
+        currentImg.prev("img").addClass("active");
+        currentDot.prev(".dot").addClass("active");
+    } else {
+        // prev.next() it's empty
+        // --> add class .active in the last img and dot
+        $(".carousel > img:last-of-type").addClass("active");
+        $(".carousel > .dots > .dot:last-of-type").addClass("active");
+    }
+};
