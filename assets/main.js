6ab058bd350015183c975df616ca0f880322ff7c
Marco Guidi <mark.88g@gmail.com>
2020-10-27 16:02:30 +0100

    realizzati dots (ref img)

--- assets/main.js
+++ assets/main.js
@@ -3,34 +3,42 @@ $("document").ready(function() {
     // CAROUSEL MOOVER
     // button for moving next
     $(".next").click(function() {
-        // current img assignment
+        // VAR ASSIGNMENT: IMG AND DOT
         var currentImg = $("img.active");
-        // remove class .active in the current img
+        var currentDot = $(".dot.active");
+        // remove class .active in the current img and in the current dot
         currentImg.removeClass("active");
+        currentDot.removeClass("active");
         if (currentImg.next("img").length) {
             // current.next() it's not empty
-            // --> add class .active in the next
+            // --> add class .active in the next img and dot
             currentImg.next("img").addClass("active");
+            currentDot.next(".dot").addClass("active");
         } else {
             // current.next() it's empty
-            // --> add class .active in the first
+            // --> add class .active in the first img and dot
             $(".carousel > img:first-of-type").addClass("active");
+            $(".carousel > .dots > .dot:first-of-type").addClass("active");
         }
     });
     // button for moving prev
     $(".prev").click(function() {
-        // current img assignment
+        // VAR ASSIGNMENT: IMG AND DOT
         var currentImg = $("img.active");
-        // remove class .active in the current img
+        var currentDot = $(".dot.active");
+        // remove class .active in the current img and dot
         currentImg.removeClass("active");
+        currentDot.removeClass("active");
         if (currentImg.prev("img").length) {
             // prev.next() it's not empty
-            // --> add class .active in the prev
+            // --> add class .active in the prev img and dot
             currentImg.prev("img").addClass("active");
+            currentDot.prev(".dot").addClass("active");
         } else {
             // prev.next() it's empty
-            // --> add class .active in the last
+            // --> add class .active in the last img and dot
             $(".carousel > img:last-of-type").addClass("active");
+            $(".carousel > .dots > .dot:last-of-type").addClass("active");
         }
     });
 });
